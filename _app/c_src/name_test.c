61f06cc1829040488e8b6b2105d69fe048c08043
niamtokik <niamtokik@localhost>
2020-07-02 05:46:01 +0000

    correct typo

--- _app/c_src/name_test.c
+++ _app/c_src/name_test.c
@@ -9,23 +9,27 @@ int tests_run = 0;
 int foo = 7;
 int bar = 4;
 
-static char * test_foo() {
+static char *
+test_foo() {
   mu_assert("error, foo != 7", foo == 7);
   return 0;
 }
 
-static char * test_bar() {
-  mu_assert("error, bar != 5", bar == 5);
+static char *
+test_bar() {
+  mu_assert("error, bar != 5", bar != 5);
   return 0;
 }
 
-static char * all_tests() {
+static char *
+all_tests() {
   mu_run_test(test_foo);
   mu_run_test(test_bar);
   return 0;
 }
 
-int main(int argc, char **argv) {
+int
+main(int argc, char **argv) {
   char *result = all_tests();
   if (result != 0) {
     printf("%s\n", result);
